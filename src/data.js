bbad890ce2c4cf5d8cbf6294f73b72d3e6981f75
Thornvik <54942355+Thornvik@users.noreply.github.com>
2020-04-20 22:54:17 +0200

    fake database

---
+++ src/data.js
@@ -0,0 +1,160 @@
+const data = [
+    {
+        "Title": "",
+        "Year": "1950",
+        "Text": "Concept of resillience first noted in psychology, \"Maternal care and mental health\" by John Bowlby WHO",
+        "Tag": "Program"
+    },
+    {
+        "Title": "",
+        "Year": "1973",
+        "Text": "\"Competence and adaptation in adult schizophrenic patients and children at risk\" by Norman Garmezy \"Resillience and Stability of Ecological Systems\" by c.s. Hoilling",
+        "Tag": "Publication"
+    },
+    {
+        "Title": "",
+        "Year": "1983",
+        "Text": "Ethiopia famine and hunger crisis",
+        "Tag": "Event"
+    },
+    {
+        "Title": "",
+        "Year": "1985",
+        "Text": "Famine Early Warning System Network (FEWSNET) created",
+        "Tag": "Program"
+    },
+    {
+        "Title": "",
+        "Year": "1992",
+        "Text": "UN Conference on Environment and Development (UNCED) in Rio, Brazil",
+        "Tag": "Event"
+    },
+    {
+        "Title": "",
+        "Year": "1995",
+        "Text": "UN Millenium Development Goals (MDGs). Sustainable Livelihoods Framework",
+        "Tag": "Event, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "1996",
+        "Text": "Mobile phones and SMS enter the mainstream",
+        "Tag": "Event, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "1999",
+        "Text": "Africa Evaluation Association fromed. Resillience Alliance formed",
+        "Tag": "Program"
+    },
+    {
+        "Title": "",
+        "Year": "2001",
+        "Text": "\"Panarchy: Understanding Transformations in Systems of Humans and Nature\" by Lance Gunderston & C.S. Hoilling",
+        "Tag": "Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2004",
+        "Text": "Facebook's website Launces. Indian Ocean tsunami",
+        "Tag": "Event"
+    },
+    {
+        "Title": "",
+        "Year": "2005",
+        "Text": "",
+        "Tag": "Event, Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2006",
+        "Text": "",
+        "Tag": "Trend, Program"
+    },
+    {
+        "Title": "",
+        "Year": "2007",
+        "Text": " ",
+        "Tag": "Program, Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2008",
+        "Text": "",
+        "Tag": "Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2009",
+        "Text": "",
+        "Tag": "Event, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2010",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework, Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2011",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework, Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2012",
+        "Text": "",
+        "Tag": "Event, Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2013",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework, Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2014",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework, Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2015",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework, Publication"
+    },
+    {
+        "Title": "",
+        "Year": "2016",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2017",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2018",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2019",
+        "Text": "",
+        "Tag": "Trend, Event, Program, Framework"
+    },
+    {
+        "Title": "",
+        "Year": "2020",
+        "Text": null,
+        "Tag": null
+    }
+]
+
+export default data
