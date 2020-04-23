2b7170f27c36f49f8ef31f60897b1b9729a3f675
Thornvik <54942355+Thornvik@users.noreply.github.com>
2020-04-23 12:28:36 +0200

    data.js added Id

--- src/data.js
+++ src/data.js
@@ -1,181 +1,181 @@
 const data = [
     {
-        "Id": "\"#1950\"",
+        "Id": "#1950",
         "Year": "1950",
         "Text": "Concept of resillience first noted in psychology, \"Maternal care and mental health\" by John Bowlby WHO",
         "Tag": "Program",
         "Link": null
     },
     {
-        "Id": "\"#1973\"",
+        "Id": "#1973",
         "Year": "1973",
         "Text": "\"Competence and adaptation in adult schizophrenic patients and children at risk\" by Norman Garmezy \"Resillience and Stability of Ecological Systems\" by c.s. Hoilling",
         "Tag": "Publication",
         "Link": null
     },
     {
-        "Id": "\"#1983\"",
+        "Id": "#1983",
         "Year": "1983",
         "Text": "Ethiopia famine and hunger crisis",
         "Tag": "Event",
         "Link": null
     },
     {
-        "Id": "\"#1985\"",
+        "Id": "#1985",
         "Year": "1985",
         "Text": "Famine Early Warning System Network (FEWSNET) created",
         "Tag": "Program",
         "Link": null
     },
     {
-        "Id": "\"#1992\"",
+        "Id": "#1992",
         "Year": "1992",
         "Text": "UN Conference on Environment and Development (UNCED) in Rio, Brazil",
         "Tag": "Event",
         "Link": null
     },
     {
-        "Id": "\"#1995\"",
+        "Id": "#1995",
         "Year": "1995",
         "Text": "UN Millenium Development Goals (MDGs). Sustainable Livelihoods Framework",
         "Tag": "Event, Framework",
         "Link": null
     },
     {
-        "Id": "\"#1996\"",
+        "Id": "#1996",
         "Year": "1996",
         "Text": "Mobile phones and SMS enter the mainstream",
         "Tag": "Event, Framework",
         "Link": null
     },
     {
-        "Id": "\"#1999\"",
+        "Id": "#1999",
         "Year": "1999",
         "Text": "Africa Evaluation Association fromed. Resillience Alliance formed",
         "Tag": "Program",
         "Link": null
     },
     {
-        "Id": "\"#2001\"",
+        "Id": "#2001",
         "Year": "2001",
         "Text": "\"Panarchy: Understanding Transformations in Systems of Humans and Nature\" by Lance Gunderston & C.S. Hoilling",
         "Tag": "Publication",
         "Link": null
     },
     {
-        "Id": "\"#2004\"",
+        "Id": "#2004",
         "Year": "2004",
         "Text": "Facebook's website Launces. Indian Ocean tsunami",
         "Tag": "Event",
         "Link": null
     },
     {
-        "Id": "\"#2005\"",
+        "Id": "#2005",
         "Year": "2005",
         "Text": "",
         "Tag": "Event, Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2006\"",
+        "Id": "#2006",
         "Year": "2006",
         "Text": "",
         "Tag": "Trend, Program",
         "Link": null
     },
     {
-        "Id": "\"#2007\"",
+        "Id": "#2007",
         "Year": "2007",
         "Text": " ",
         "Tag": "Program, Publication",
         "Link": null
     },
     {
-        "Id": "\"#2008\"",
+        "Id": "#2008",
         "Year": "2008",
         "Text": "",
         "Tag": "Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2009\"",
+        "Id": "#2009",
         "Year": "2009",
         "Text": "",
         "Tag": "Event, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2010\"",
+        "Id": "#2010",
         "Year": "2010",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework, Publication",
         "Link": null
     },
     {
-        "Id": "\"#2011\"",
+        "Id": "#2011",
         "Year": "2011",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework, Publication",
         "Link": null
     },
     {
-        "Id": "\"#2012\"",
+        "Id": "#2012",
         "Year": "2012",
         "Text": "",
         "Tag": "Event, Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2013\"",
+        "Id": "#2013",
         "Year": "2013",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework, Publication",
         "Link": null
     },
     {
-        "Id": "\"#2014\"",
+        "Id": "#2014",
         "Year": "2014",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework, Publication",
         "Link": null
     },
     {
-        "Id": "\"#2015\"",
+        "Id": "#2015",
         "Year": "2015",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework, Publication",
         "Link": null
     },
     {
-        "Id": "\"#2016\"",
+        "Id": "#2016",
         "Year": "2016",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2017\"",
+        "Id": "#2017",
         "Year": "2017",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2018\"",
+        "Id": "#2018",
         "Year": "2018",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2019\"",
+        "Id": "#2019",
         "Year": "2019",
         "Text": "",
         "Tag": "Trend, Event, Program, Framework",
         "Link": null
     },
     {
-        "Id": "\"#2020\"",
+        "Id": "#2020",
         "Year": "2020",
         "Text": null,
         "Tag": null,
